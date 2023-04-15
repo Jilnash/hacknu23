7873accb27a1e59ad2be5aaa4e76bae24aab2403
Aydar Kanatov <aydar-kanatov@mail.ru>
2023-04-15 10:08:00 +0600

    first commit

---
+++ src/test/java/com/jilnash/hacknu23/Hacknu232ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jilnash.hacknu23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hacknu232ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
